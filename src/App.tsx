e3a73ca0e5236306207ea69330f52fa3aa2495f0
Denizhan Gonen <gonendenizhan@gmail.com>
2022-03-11 17:26:28 +0300

    initial settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const themeLight = createTheme({
+  palette: {
+    background: {
+      default: 'rgb(217, 85, 80)',
+    },
+    text: {
+      primary: 'white',
+    },
+  },
+  typography: {
+    fontSize: 24,
+  },
+});
 
 function App() {
-  return <div className="App">asd</div>;
+  return (
+    <ThemeProvider theme={themeLight}>
+      <CssBaseline />
+      <div className="App">asdasdasd</div>;
+    </ThemeProvider>
+  );
 }
 
 export default App;
